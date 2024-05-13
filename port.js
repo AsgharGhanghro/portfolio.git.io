c0c603a72e3b407b6e8e9a66b45a0f85cdcf7ad7
AsgharGhanghro <166389875+AsgharGhanghro@users.noreply.github.com>
2024-05-13 23:15:25 +0500

    job
    job portfolio

---
+++ port.js
@@ -0,0 +1,11 @@
+
+// let BTN=document.querySelector("#cv");
+// BTN.addEventListener("click",()=>{
+//    window.open("Capture1.PNG","_blank");
+// });
+
+
+// let dev =document.querySelector("#btn");
+// dev.addEventListener("click",()=>{
+//    window.open("index.html","_blank");
+// });
